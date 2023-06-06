d950e5fe257f9c5fee572186fb45057aa1d0766b
Johnny da Silva Oliveira <johnny_s.o@hotmail.com>
2023-06-06 18:59:41 -0300

    Adicionado componenet Header

---
+++ src/components/header/header.jsx
@@ -0,0 +1,18 @@
+import {React} from 'react';
+
+import './header.css'
+
+export default function Header(){
+    return (
+        <>
+        <div className="headerMenu">
+            <h2 className="title">Jujutsu Kaisen</h2>
+            <ul className="headerList">
+                <li>Comunidade</li>
+                <li>Sistema</li>
+                <li>Cadastro</li>
+            </ul>
+        </div>
+        </>
+    )
+}
