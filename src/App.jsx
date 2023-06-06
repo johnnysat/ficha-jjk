cdc7e16c5e90c8c065d991e040db1ef864dfb86a
Johnny da Silva Oliveira <johnny_s.o@hotmail.com>
2023-06-06 19:12:44 -0300

    Adicionado conteudo principal

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import { React } from 'react'
 import Header from './components/header/header'
+import Main from './components/main/main'
+
+import './App.css'
+
 
 function App() {
   return (
-    <>
+    <div className="bodyBox">
     <Header />
-    </>
+    <Main />
+    </div>
   )
 }
 
---
+++ src/components/main/main.jsx
@@ -0,0 +1,17 @@
+import { React } from 'react';
+
+import './main.css'
+
+export default function Main() {
+    return (
+        <>
+            <div className="mainBox">
+                <h1>UM PRG DE JUJUTSU KAISEN</h1>
+                <p>Jujutsu Kaisen é um mangá japonês escrito e ilustrado por Gege Akutami,
+                    serializado na Weekly Shōnen Jump desde 5 de março de 2018. Os capítulos individuais
+                    são compilados em volume tankōbon, e publicados pela Shueisha desde julho de 2018.
+                    A série é licenciada e publicada no Brasil pela editora Panini.</p>
+            </div>
+        </>
+    )
+}
