2b4eba10ef5086c9745e058b5f29860c57406853
Johnny da Silva Oliveira <johnny_s.o@hotmail.com>
2023-06-08 12:20:57 -0300

    Adicionado card ao lado do main content

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 
 import Header from './components/header/header'
 import Main from './components/main/main'
+import Card from './components/card/card'
 
 
 
@@ -10,7 +11,10 @@ function App() {
   return (
     <div className="bodyBox">
     <Header />
-    <Main />
+      <section className="container">
+      <Main />
+      <Card />
+      </section>
     </div>
   )
 }
---
+++ src/components/card/card.jsx
@@ -0,0 +1,18 @@
+import {React} from 'react';
+
+import './card.css'
+
+export default function Card(){
+    return (
+        <>
+        <div className="card">
+            <ul>
+                <li>teste</li>
+                <li>teste</li>
+                <li>teste</li>
+                <li>teste</li>
+            </ul>
+        </div>
+        </>
+    )
+}
